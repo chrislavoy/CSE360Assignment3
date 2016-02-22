1b6c2bd6925f8a0a072cccf558c28194e321a645
Chris LaVoy <clavoy@asu.edu>
2016-02-22 13:56:37 -0700

    First project version

--- Calculator.java
+++ Calculator.java
@@ -1,33 +1,53 @@
 package cse360assign3;
 
+/** A basic calculator with history 
+ * @author Chris LaVoy
+ * @version Feb 21, 2016
+ * */
 public class Calculator {
 
+	/** A value to hold the calculated to */
 	private int total;
 	
+	/** Sets total to 0 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/** Returns the total */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/** Adds a value to the total
+	 * @param value		the value to add to the total 
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/** Subtracts a value from the total
+	 * @param value		the value to subtract from the total
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	/** Multiplies the total by a value
+	 * @param value		the value to multiply the total by 
+	 */
 	public void multiply (int value) {
 		
 	}
 	
+	/**	Divides the total by a value
+	 * @param value		the value to divide the total by
+	 */
 	public void divide (int value) {
 		
 	}
 	
+	/** Returns the history of operations done by the calculator as a String */
 	public String getHistory () {
 		return "";
 	}
---
+++ CalculatorTest.java
@@ -0,0 +1,46 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cse360assign2.OrderedIntList;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator calculator = new Calculator();
+		assertNotNull(calculator);
+	}
+	
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+}
